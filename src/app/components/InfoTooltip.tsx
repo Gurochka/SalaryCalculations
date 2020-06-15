7354024dff22bb272eb21d10b28103ea868f8015
gurochka <natali.ananyeva@gmail.com>
2020-06-15 20:19:54 +0400

    add tooltip

--- src/app/components/InfoTooltip.tsx
+++ src/app/components/InfoTooltip.tsx
@@ -1,12 +1,37 @@
-import * as React from 'react';
+import React, { SyntheticEvent } from 'react';
 import QuestionCircle from './Icons/QuestionCircle'
 import CloseCircle from './Icons/CloseCircle'
 
-const InfoTooltip: React.FunctionComponent<{}> = (props) => {
-  return (
-    <div className="info-tooltip">
-      <QuestionCircle />
-    </div>
-  )
+interface State {
+  showTooltip: boolean;
 }
-export default InfoTooltip;
+
+export default class InfoTooltip extends React.Component {
+  state: State = {
+    showTooltip: false
+  }
+
+  handleCloseTooltip = (e:SyntheticEvent) => {
+    e.preventDefault();
+    this.setState({ showTooltip: false })
+  }
+
+  render(){
+    const { showTooltip } = this.state;
+
+    return (
+      <div className="info-tooltip" onMouseEnter={() => this.setState({ showTooltip: true})}>
+        { !showTooltip && <QuestionCircle /> }
+        { showTooltip && (
+            <>
+              <CloseCircle onClick={this.handleCloseTooltip} />
+              <div className="info-tooltip-tooltip">
+                {this.props.children}
+              </div>
+            </>
+          ) 
+        }
+      </div>
+    )
+  }
+}
